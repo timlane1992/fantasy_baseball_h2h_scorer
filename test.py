b5e5ec11ad98e56d92731df428fb6a1427c37961
tlane <tlane@cray.com>
2018-07-10 18:08:34 -0500

    Use unit testing approach. Move test data into its own directory.

--- test.py
+++ test.py
@@ -1,51 +1,84 @@
 #!/usr/bin/env python
 
-import logging
-import sys
-from datetime import datetime, timedelta
+import json
+import os
+import tempfile
+import unittest
+from datetime import datetime
 
 import bref_scraper
 import scorer
 from lineup import Lineup
 
-
-# Set beginning and end times
+# Date format used for test data
 DATE_FORMAT = '%m-%d-%Y'
-START_DATE = datetime.strptime('04-30-2018', DATE_FORMAT)
-END_DATE = datetime.strptime('05-06-2018', DATE_FORMAT)
 
-# Load head-to-head lineup
-logging.basicConfig(level=logging.DEBUG)
-hth_lineup = Lineup('lineup_cfg_file.json')
-hitters = hth_lineup.get_hitters()
-pitchers = hth_lineup.get_pitchers()
-print hth_lineup
 
-# Get game logs for every player in the lineup
-game_log_holder = dict()
-for player in hth_lineup:
-    game_log_holder[player.baseball_ref_id] = bref_scraper.get_game_logs(player.baseball_ref_id,
-                                                                         START_DATE,
-                                                                         END_DATE,
-                                                                         player in hitters)
 
-# Get h2h scores for hitters
-hitter_sum = 0
-print '-' * 30, 'HITTER SCORES', '-' * 30
-for hitter in hitters:
-    game_log_list = game_log_holder[hitter.baseball_ref_id]
-    score = scorer.get_hitter_score(game_log_list)
-    print 'Hitter: {player}, score: {score}'.format(player=hitter, score=score)
-    hitter_sum += score
-print 'TOTAL SCORE - HITTERS:', hitter_sum
+class WeekFive2018TestCase(unittest.TestCase):
+    def setUp(self):
+        self.lineup_file = None
+        self.start_date = None
+        self.end_date = None
+        self.exp_hit_score = None
+        self.exp_pitch_score = None
+        self.exp_total_score = None
 
-# Get h2h scores for pitchers
-pitcher_sum = 0
-print '-' * 30, 'PITCHER SCORES', '-' * 30
-for pitcher in pitchers:
-    game_log_list = game_log_holder[pitcher.baseball_ref_id]
-    score = scorer.get_pitcher_score(game_log_list)
-    print 'Pitcher: {player}, score: {score}'.format(player=pitcher, score=score)
-    pitcher_sum += score
-print 'TOTAL SCORE - PITCHERS:', pitcher_sum
-print 'TOTAL SCORE - ALL:', pitcher_sum + hitter_sum
+        # Load in test data
+        test_data = None
+        with open(os.path.join('test_lineups/2018_week_5_test_data.json')) as test_data_file:
+            test_data = json.load(test_data_file)
+
+        # Create file containing lineup JSON data
+        self.lineup_file = tempfile.NamedTemporaryFile('w', delete=False)
+        json.dump(test_data['lineup'], self.lineup_file)
+        self.lineup_file.close()
+
+        # Get start/end dates and expected scores
+        self.start_date = datetime.strptime(test_data['start_date'], DATE_FORMAT)
+        self.end_date = datetime.strptime(test_data['end_date'], DATE_FORMAT)
+        self.exp_hit_score = test_data['expected_hitters_score']
+        self.exp_pitch_score = test_data['expected_pitchers_score']
+        self.exp_total_score = self.exp_hit_score + self.exp_pitch_score
+
+
+    def test_scoring(self):
+
+        # Load H2H lineup
+        hth_lineup = Lineup(self.lineup_file.name)
+        hitters = hth_lineup.get_hitters()
+        pitchers = hth_lineup.get_pitchers()
+
+        # Get game logs for every player in the lineup
+        game_log_holder = dict()
+        for player in hth_lineup:
+            game_log_holder[player.baseball_ref_id] = bref_scraper.get_game_logs(player.baseball_ref_id,
+                                                                                 self.start_date,
+                                                                                 self.end_date,
+                                                                                 player in hitters)
+
+        # Get h2h scores for hitters
+        hitter_sum = 0
+        for hitter in hitters:
+            game_log_list = game_log_holder[hitter.baseball_ref_id]
+            score = scorer.get_hitter_score(game_log_list)
+            hitter_sum += score
+
+        # Get h2h scores for pitchers
+        pitcher_sum = 0
+        for pitcher in pitchers:
+            game_log_list = game_log_holder[pitcher.baseball_ref_id]
+            score = scorer.get_pitcher_score(game_log_list)
+            pitcher_sum += score
+
+        self.assertEqual(self.exp_hit_score, hitter_sum)
+        self.assertEqual(self.exp_pitch_score, pitcher_sum)
+        self.assertEqual(self.exp_total_score, pitcher_sum + hitter_sum)
+
+
+    def tearDown(self):
+        os.unlink(self.lineup_file.name)
+
+
+if __name__ == '__main__':
+    unittest.main()
