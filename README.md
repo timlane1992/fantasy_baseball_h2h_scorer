9560cbe65c2e65faed82655d001a93d53e5e9a93
tlane <timlane1992@gmail.com>
2019-03-11 23:17:23 -0500

    Add package stuff.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # TODO:
 * Make users refer to players by their baseball reference ID in lineup data
 * Package this thing in a standard way (pip-installable)
+** Package dependencies
 * Make this thing more testable?
 * Eventually, provide mechanism for storing, retrieving, and displaying/interacting with results for individual players and teams
 * Something that takes in a full team and spits out the optimal lineup? Could be done with respect to the past or (if projection data is available) to the future.
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='fantasy_baseball_h2h_scorer',
+	  version='0.1',
+	  description='Automated scoring for http://fobl00.baseball.cbssports.com/',
+	  long_description='Python package to make up for the lack of automated scoring provide by http://fobl00.baseball.cbssports.com/',
+	  classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+      ],
+      keywords='fantasy baseball scorer',
+      url='https://github.com/timlane1992/fantasy_baseball_h2h_scorer',
+      author='Tim Lane',
+      author_email='timlane1992@gmail.com',
+      license='MIT',
+      install_requires=[  # TODO: this
+      ])
